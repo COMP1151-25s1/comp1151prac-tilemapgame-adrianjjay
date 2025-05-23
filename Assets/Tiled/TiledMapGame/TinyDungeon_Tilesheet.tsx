21aea8e8e8f585aefa79d1969b6dc91c709dbaca
adrianjjay <adrianjay.asuncion@students.mq.edu.au>
2025-05-23 14:46:27 +1000

    Adding hazards for the player

--- Assets/Tiled/TiledMapGame/TinyDungeon_Tilesheet.tsx
+++ Assets/Tiled/TiledMapGame/TinyDungeon_Tilesheet.tsx
@@ -1,4 +1,41 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TinyDun" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <properties>
+  <property name="unity:IsTrigger" value="true"/>
+  <property name="unity:layer" value="Hazard"/>
+ </properties>
  <image source="tilemap.png" width="203" height="186"/>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="4" x="1.125" y="1.0625">
+    <polygon points="-1.125,-1.0625 -1.125,14.9375 14.875,14.9375 2.875,10.9375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
 </tileset>
